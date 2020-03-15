7a921df8ac462016105046c333ec6ba2b6d5bebd
Noel Gonzalez <43417136+Ng07886@users.noreply.github.com>
2020-03-15 00:52:35 -0400

    Create asyncAwait.js

---
+++ asyncAwait.js
@@ -0,0 +1,19 @@
+
+
+async function getData() {
+    let taco= new Promise((steak,chicken) => {
+    setTimeout(() => steak('done'),4000);
+    });
+    
+    let finished = await taco;
+    console.log(finished);
+    }
+    
+    getData();
+    
+    /* The first thing you notice is that the fuction has a async in front of it. 
+       this lets the compiler know that it will havve a promise it will have to return to.
+       Instead of writing .then to wait you write 'await' to let the compiler know it needs
+       to wait for what every the await is attached to to finish and then it will continue.
+       If you ran this you will have to wait 4 sec for beacue the compiler is waiting due to the await 
+       in front of taco. */
