669f34e1b68b74df743fb4e4f5bdff45d8588f94
Noel Gonzalez <43417136+Ng07886@users.noreply.github.com>
2020-08-16 22:22:56 -0400

    Update asyncAwait.js

--- asyncAwait.js
+++ asyncAwait.js
@@ -14,6 +14,6 @@ async function getData() {
     /* The first thing you notice is that the fuction has a async in front of it. 
        this lets the compiler know that it will havve a promise it will have to return to.
        Instead of writing .then to wait you write 'await' to let the compiler know it needs
-       to wait for what every the await is attached to to finish and then it will continue.
-       If you ran this you will have to wait 4 sec for beacue the compiler is waiting due to the await 
+       to wait for whatever the await is attached to finish and then it will continue.
+       If you ran this you will have to wait 4 sec beacue the compiler is waiting due to the await 
        in front of taco. */
